04dfdca0ab5a3c6f4b61474958dc5358b3b24326
arturoceballos <aceballos118@gmail.com>
2015-01-29 13:28:50 -0800

    Added a php file for a parallax section on the page

---
+++ wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/functions/m-parallax.php
@@ -0,0 +1,25 @@
+<?php global $nature_mt; ?>
+<!-- Parallax -->
+    <section id="newsletter" class="newsletter">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12">
+                    <div class="newsletter-box">
+                                <?php if(isset($nature_mt['newsletter_text']) && $nature_mt['newsletter_text'] != '') { ?>
+                                <?php echo $nature_mt['newsletter_text'];?>
+                                <?php } ?>
+                        <div class="newsletter-form">
+                            <div class="newsletter-form-box">
+                                <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid asperiores cum, dignissimos ducimus eligendi esse iste mollitia optio perspiciatis placeat quae reiciendis repellat repellendus sapiente similique veniam vitae voluptatibus.
+                                </p>
+                            </div>
+                            <div class="mess center">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- Parallax end -->
