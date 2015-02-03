af88f23871e2e66bc3a8f8d8145af7eab180505f
arturoceballos <aceballos118@gmail.com>
2015-02-03 15:09:14 -0800

    created about-2.php as the about section template. No changes made to bootstrap.css

---
+++ wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/about-2.php
@@ -0,0 +1,17 @@
+<section id="<?php echo $post->post_name;?>" class="<?php echo $post->post_name;?>">
+        <div class="container-fluid">
+            <!-- section title -->
+            <div class="row-fluid">
+                <div class="span12">
+                    <div class="section-title">
+                        <h2>
+                            <span><?php $top_title = get_post_meta($post->ID, 'top_title', true); 
+                                if($top_title != '') echo $top_title; else the_title();?></span>
+                        </h2>
+                        <p><?php echo get_post_meta( $post->ID, '_cmb_p_sub_title', true ); ?></p>
+                    </div>
+                </div>
+            </div>
+			<h5><?php global $more; $more = 0; the_content('');?></h5>
+        </div><!-- end of container --> 
+</section><!-- end of sections -->
