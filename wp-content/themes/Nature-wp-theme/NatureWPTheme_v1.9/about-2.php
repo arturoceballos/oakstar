c6a77032cb1e36e0ff1084df74b0005264768905
arturoceballos <aceballos118@gmail.com>
2015-02-05 11:36:30 -0800

    removed unnecessary divs and centered everything on page

--- wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/about-2.php
+++ wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/about-2.php
@@ -1,18 +1,16 @@
 <section id="<?php echo $post->post_name;?>" class="<?php echo $post->post_name;?>">
-        <div class="container-fluid">
+        <div class="container-fluid-2">
             <!-- section title -->
             <div class="row-fluid">
-                <div class="span12">
                     <div class="section-title">
                         <h2>
                             <span><?php $top_title = get_post_meta($post->ID, 'top_title', true); 
                                 if($top_title != '') echo $top_title; else the_title();?></span>
                         </h2>
-                        <p><?php echo get_post_meta( $post->ID, '_cmb_p_sub_title', true ); ?></p>
+                        <h3><?php echo get_post_meta( $post->ID, '_cmb_p_sub_title', true ); ?></h3>
                     </div>
-                </div>
             </div>
-            <div class="col-md-8 col-md-offset-2" style="text-align:center;">
+            <div class=".center-block" style="text-align:center;">
                 <h5><?php global $more; $more = 0; the_content('');?></h5>
             </div>
         </div><!-- end of container --> 
