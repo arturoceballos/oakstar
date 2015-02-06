7d800efd0fce1f1bc3d34ceca2cc0b07dfcadf6d
arturoceballos <aceballos118@gmail.com>
2015-02-06 14:48:20 -0800

    Added flipcard js

--- wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/js/scripts.js
+++ wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/js/scripts.js
@@ -118,5 +118,26 @@ $('#newsletter-form').submit(function() {
 
 });
 
+ //you can specify an options when calling flip() function
+var options = {
+       alwaysOneDirection: 'false', //values: 'true' or 'false', indicates if flipping card always in one direction
+       direction: 'lr', //values: 'lr' (left to right), 'tb'(top to bottom), 'rl' and 'bt'
+       speed: '500ms', //specify animation duration
+       timingfunction: 'ease', //built-in timing function list (see it below) or customized cubic-beizer
+       onflipping: function(dir, transDir) //this event will be triggered before animation
+       {
+           //dir is direction e.g. 'lr', 'tb'
+           //transDir means transforming from front to back (value: 'fb') or back to front (value: 'bf')
+           console.log("onflipping event"); 
+       },
+       onflipped: function(dir, transDir) //this event will be triggered after animation
+       {
+           //dir is direction e.g. 'lr', 'tb'
+           //transDir means transforming from front to back (value: 'fb') or back to front (value: 'bf')
+           console.log("onflipped event"); 
+       }
+}
+$(".card-container").flip(options);
+
 
 });
