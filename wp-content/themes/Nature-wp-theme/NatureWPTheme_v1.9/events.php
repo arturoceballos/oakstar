175f729052819827f46549133c7243366d4f6213
arturoceballos <aceballos118@gmail.com>
2015-02-12 19:18:36 -0800

    finsished events page

--- wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/events.php
+++ wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/events.php
@@ -1,17 +1,17 @@
-<div class="row <?php echo $post->post_name;?>" id="<?php echo $post->post_name;?>">
-    <div class="container-fluid">
+<div class="row-fluid <?php echo $post->post_name;?>" id="<?php echo $post->post_name;?>">
+    <div class="container-fluid-events">
         <!-- Section Title -->
         <div class="section-title">
             <h2>
             <span><?php $top_title = get_post_meta($post->ID, 'top_title', true);
                 if($top_title != '') echo $top_title; else the_title();?></span>
             </h2>
-            <p><?php echo get_post_meta( $post->ID, '_cmb_p_sub_title', true ); ?></p>
+            <h3><?php echo get_post_meta( $post->ID, '_cmb_p_sub_title', true ); ?></h3>
         </div>
         <!--End Section Title-->
 
         <!--Content-->
-        <div id="container" class="services-section"  data-masonry-options='{ "columnWidth": 20, "itemSelector": ".event" }'>
+        <div class="events-content">
             <?php
 
             //get the event custom post types
@@ -19,14 +19,15 @@
             $args = array(
                 'post_type'        => $type,
                 'post_status'      => 'publish',
-                'order'            => 'date',
-                'date'             => 'date',
-                'orderby'          => 'title',
+                'order'            => 'ASC',
+                // 'date'             => 'date',
+                'orderby'          => 'meta_value_num',
+                'meta key'         => 'event-date',
                 'posts_per_page'   => -1,
-                "title" => 'SERVICE TITLE',
-                "text" => 'SERVICE DETAILS',
-                "image" => 'SERVICE IMAGE',
-                "url" => 'SERVICE BUTTON URL',
+                // "title" => 'SERVICE TITLE',
+                // "text" => 'SERVICE DETAILS',
+                // "image" => 'SERVICE IMAGE',
+                // "url" => 'SERVICE BUTTON URL',
             );
 
             $my_query = null;
@@ -37,23 +38,13 @@
                 <?php
                 $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 500,500 ), false, '' );
                 ?>
-                        <div class="wow animated bounceInUp event pull-left">
 
-
-
-                            <div class="col-sm-12 col-md-4 col-lg-3 service-span">
-                                <div class="service-box">
-                                    <img class="img-circle" src="<?php echo $src[0]; ?>" alt="'.$title.'">
-                                    <h2><?php the_title(); ?></h2>
-                                    <p><?php the_content();?></p>
-
-                                </div>
-                            </div>
-
-                            <?php echo types_render_field('event-date', array('output' => 'raw'));?>
-                                        
-
-                        </div>
+                    <div class="events-row pad">
+                        <img src="<?php echo $src[0]; ?>" alt="'.$title.'">
+                        <h2><?php the_title(); ?></h2>
+                        <h3><?php echo date('D. F jS, Y', types_render_field('event-date', array('output' => 'raw')));?></h3>
+                        <p><?php the_content();?></p>
+                    </div>
                 
             <?php endwhile; endif; wp_reset_postdata(); ?>
         </div>
