42306b0aaa0a2c931faf20b0d63d92f9e460197d
arturoceballos <aceballos118@gmail.com>
2015-02-19 09:50:37 -0800

    Made about section respnsive

--- wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/about-2.php
+++ wp-content/themes/Nature-wp-theme/NatureWPTheme_v1.9/about-2.php
@@ -1,17 +1,21 @@
-<section id="<?php echo $post->post_name;?>" class="<?php echo $post->post_name;?>">
-        <div class="container-fluid-2">
-            <!-- section title -->
-            <div class="row-fluid">
-                    <div class="section-title">
-                        <h2>
-                            <span><?php $top_title = get_post_meta($post->ID, 'top_title', true); 
-                                if($top_title != '') echo $top_title; else the_title();?></span>
-                        </h2>
-                        <h3><?php echo get_post_meta( $post->ID, '_cmb_p_sub_title', true ); ?></h3>
-                    </div>
+<div class="row-fluid <?php echo $post->post_name;?>" id="<?php echo $post->post_name;?>">
+    <div class="container-fluid-about">
+        <!-- section title -->
+                <?php
+                $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 400,400 ), false, '' );
+                ?>
+                <img src="<?php echo $src[0]; ?>" alt="'.$title.'" class="about-img">
+            <div class="about-content">
+                <div class="section-title">
+                    <h2>
+                    <span><?php $top_title = get_post_meta($post->ID, 'top_title', true);
+                        if($top_title != '') echo $top_title; else the_title();?></span>
+                    </h2>
+                    <h3><?php echo get_post_meta( $post->ID, '_cmb_p_sub_title', true ); ?></h3>
+                </div>
+                <div class=".center-block" style="text-align:center;">
+                    <h5><?php global $more; $more = 0; the_content('');?></h5>
+                </div>
             </div>
-            <div class=".center-block" style="text-align:center;">
-                <h5><?php global $more; $more = 0; the_content('');?></h5>
-            </div>
-        </div><!-- end of container --> 
-</section><!-- end of sections -->
+    </div><!-- end of container -->
+</div>
